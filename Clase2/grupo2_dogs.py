8485cc9a61307fe578c0baf9c0e9f96d66ca882b
Alejandro JS <alejandro97JS@gmail.com>
2026-03-06 22:18:12 +0100

    Include improvement suggestions in DogClient class
    Added suggestions for code improvements and style enhancements.

--- Clase2/grupo2_dogs.py
+++ Clase2/grupo2_dogs.py
@@ -1,5 +1,10 @@
 # Tenemos que crear una clase para el cliente
 # Evaluar la documentacion de la api y evaluar que podemos utilizar en nuestra clase cliente.abs
+
+# Alejandro - Incluyo comentarios de mejora a lo largo del código, comenzando por # SUGGESTION.
+# Es solo un punto, y es una recomendación de estilo que, de hecho, es opinable, pero personalmente
+# lo vería más elegante como os indico a continuación. En general, está bastante guay lo que
+# hicisteis.
 import requests
 
 class DogClient:
@@ -7,13 +12,21 @@ class DogClient:
         self.base_url = "https://dog.ceo/api/breeds/"
 
     def __get(self, url: str):
+        # SUGGESTION: Hubiese quedado más elegante y más útil a largo plazo definir
+        # este método como def __get(self, url:str, endpoint:str) y hacer aquí 
+        # la concatenación de strings urls + endpoint.
         response = requests.get(url)
         return response.json()
     
     def get_list(self):
+        # SUGGESTION: Con el cambio anterior, se llamaría con 
+        # self.__get(self.base_url, "list/all")
         return self.__get(self.base_url + "list/all")
         
     def get_random_dogs(self, number_of_dogs: int = 1):
+        # SUGGESTION: Con el cambio anterior, se llamaría con 
+        # self.__get(self.base_url, f"image/random/{number_of_dogs}") --> Ver funcionamiento
+        # de los f-strings en Python.
         return self.__get(self.base_url + "image/random/" + str(number_of_dogs))
     
     def find_breeds_by_letter(self, letter: str):
